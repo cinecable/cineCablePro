e7c841555dd6137733a0fc1e623ea900105f5c15
metalkarlos <Metalkarlos0923>
2014-04-23 22:23:51 -0500

    metodos de persona

---
+++ cineCablePro/src/bo/negocio/PersonaBO.java
@@ -0,0 +1,54 @@
+package bo.negocio;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import pojo.annotations.Persona;
+import util.HibernateUtil;
+
+import dao.datos.PersonaDAO;
+
+public class PersonaBO {
+
+	private PersonaDAO personaDAO;
+	
+	public PersonaBO() {
+		personaDAO = new PersonaDAO();
+	}
+	
+	public Persona getPersonaById(int idpersona) throws Exception {
+		Persona persona = null;
+		Session session = null;
+		
+		try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            persona = personaDAO.getPersonaById(session, idpersona);
+        }
+        catch(Exception ex){
+            throw new Exception(ex);
+        }
+        finally{
+            session.close();
+        }
+		
+		return persona;
+	}
+	
+	public List<Persona> lisPersonaByPage(String[] nombres, int idarea, int pageSize, int pageNumber, int args[]) throws RuntimeException{
+		List<Persona> lisPersona = null;
+		Session session = null;
+		
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			lisPersona = personaDAO.lisPersonaByPage(session, nombres, idarea, pageSize, pageNumber, args);
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new RuntimeException();
+		}finally{
+			session.close();
+		}
+		
+		return lisPersona;
+	}
+}
---
+++ cineCablePro/src/dao/datos/PersonaDAO.java
@@ -0,0 +1,94 @@
+package dao.datos;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import pojo.annotations.Persona;
+
+public class PersonaDAO {
+
+	public Persona getPersonaById(Session session, int idpersona) throws Exception {
+		Persona persona = null;
+		
+		String hql = " from Persona ";
+		hql += " where idpersona = :idpersona ";
+		
+		Query query = session.createQuery(hql)
+				.setInteger("idpersona", idpersona);
+		
+		persona = (Persona) query.uniqueResult();
+		
+		return persona;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Persona> lisPersonaByPage(Session session, String[] nombres, int idarea, int pageSize, int pageNumber, int[] args) throws Exception {
+		List<Persona> lisPersona = null;
+		
+		Criteria criteria = session.createCriteria(Persona.class)
+		.add( Restrictions.eq("idarea", idarea));
+		
+		if(nombres != null && nombres.length > 0){
+			String query = "(";
+			for(int i=0;i<nombres.length;i++)
+			{
+				query += "(lower({alias}.apellido1) like lower('%"+nombres[i]+"%') or ";
+				query += "lower({alias}.apellido2) like lower('%"+nombres[i]+"%') or ";
+				query += "lower({alias}.nombre1) like lower('%"+nombres[i]+"%') or ";
+				query += "lower({alias}.nombre2) like lower('%"+nombres[i]+"%')) ";
+				if(i<nombres.length-1){
+					query += "and ";
+				}
+			}
+			query += ")";
+			
+			criteria.add(Restrictions.sqlRestriction(query));
+		}
+		
+		criteria.addOrder(Order.asc("apellido1"))
+		.addOrder(Order.asc("apellido2"))
+		.addOrder(Order.asc("nombre1"))
+		.addOrder(Order.asc("nombre2"))
+		.setMaxResults(pageSize)
+		.setFirstResult(pageNumber);
+		
+		lisPersona = (List<Persona>) criteria.list();
+		
+		if(lisPersona != null && lisPersona.size() > 0){
+			Criteria criteriaCount = session.createCriteria(Persona.class)
+			.setProjection( Projections.rowCount())
+			.add( Restrictions.eq("idarea", idarea));
+			
+			if(nombres != null && nombres.length > 0){
+				String query = "(";
+				for(int i=0;i<nombres.length;i++)
+				{
+					query += "(lower({alias}.apellido1) like lower('%"+nombres[i]+"%') or ";
+					query += "lower({alias}.apellido2) like lower('%"+nombres[i]+"%') or ";
+					query += "lower({alias}.nombre1) like lower('%"+nombres[i]+"%') or ";
+					query += "lower({alias}.nombre2) like lower('%"+nombres[i]+"%')) ";
+					if(i<nombres.length-1){
+						query += "and ";
+					}
+				}
+				query += ")";
+				
+				criteriaCount.add(Restrictions.sqlRestriction(query));
+			}
+			
+			Object object = criteriaCount.uniqueResult();
+			int count = (object==null?0:Integer.parseInt(object.toString()));
+			args[0] = count;
+		} else {
+			args[0] = 0;
+		}
+		
+		return lisPersona;
+	}
+}
